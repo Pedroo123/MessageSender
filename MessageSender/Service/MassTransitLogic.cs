9de793b94f7f5aeda78b95465e3f64b8ef5411e4
Pedroo123 <pedroaugusto466@gmail.com>
2021-12-02 20:45:54 -0300

    Adiciona GitIgnore, e Inicia logica do Masstransit

--- MessageSender/Service/MassTransitLogic.cs
+++ MessageSender/Service/MassTransitLogic.cs
@@ -1,7 +1,26 @@
-﻿namespace MessageSender
-{
-    public class Class1
-    {
+﻿using Azure.Messaging.ServiceBus;
+using MessageSender.Models;
+using Newtonsoft.Json;
 
+namespace MessageSender
+{
+    internal class MassTransitLogic
+    {
+        static ServiceBusClient _client;
+        static ServiceBusSender _sender;
+
+        /*
+            Place documentation here
+         */
+        private async Task SendMessageQueue(object body, string connectionString, string queueName, string urnInfo)
+        {
+            _client = new ServiceBusClient(connectionString);
+
+            _sender = _client.CreateSender(queueName);
+
+            var request = JsonConvert.SerializeObject(body);
+            
+            await _sender.SendMessageAsync(request);
+        }
     }
 }
